8cfdbae8a07423058f26f6ecd857e9872488b73a
jjstyles0001 <97764916+0ptiKali1usion@users.noreply.github.com>
2024-03-29 20:28:41 -0700

    Update readaloud_get_speech.sh

--- readaloud_get_speech.sh
+++ readaloud_get_speech.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 wget "https://support.readaloud.app/read-aloud/speak/en/$1?q=$2&t=7dbe619ad03c632eb053cc6ef16c4fbb98260b6e" 
+# oops. sleep needed to go here. i forgot.
+sleep 5
 #curl -OJLvk "https://support.readaloud.app/read-aloud/speak/en/$1?q=$2&t=7dbe619ad03c632eb053cc6ef16c4fbb98260b6e" 
